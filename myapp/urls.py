52f85c4f6131653a1feee11896270e830490a9ae
Baja023 <nik.churakov92@mail.ru>
2025-03-05 13:38:15 +0300

    v2

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
 from .views import data_view, test_view
+from . import views
 
 urlpatterns = [
     path('data/', data_view, name='data'),
     path('test/', test_view, name='test'),
+]
+from django.urls import path
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('services/', views.services, name='services'),
+    path('contact/', views.contact, name='contact'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,21 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
 
 def data_view(request):
     return HttpResponse("Это страница DATA с определённым содержимым.")
 
 def test_view(request):
     return HttpResponse("Это страница TEST с другим содержимым.")
+
+def index(request):
+    return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def services(request):
+    return render(request, 'services.html')
+
+def contact(request):
+    return render(request, 'contact.html')
